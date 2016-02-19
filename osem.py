745bd6bb29a8e4fb7210e1addec6a03030cd69d7
kibo35 <kibo35@gmail.com>
2016-02-20 00:52:05 +0900

    Added files via upload

--- osem.py
+++ osem.py
@@ -9,20 +9,23 @@ from skimage.io import imread
 from skimage import data_dir
 from skimage.transform import radon, rescale, iradon
 from skimage.draw import circle
-from scipy.ndimage import gaussian_filter1d
+from scipy.ndimage import gaussian_filter
 
 parser = argparse.ArgumentParser(description='Simulation of OSEM')
 parser.add_argument('--count', '-c', default=2e6, type=float,
                     help='slice total count. Poisson noise equivalent to COUNT is added to sinogram. If COUNT is zero, no noise is added to sinogram (true).')
-parser.add_argument('--niter', '-i', default=8, type=int,
+parser.add_argument('--niter', '-i', default=10, type=int,
                     help='number of iteration')
 parser.add_argument('--nsub', '-s', default=10, type=int,
                     help='number of subset')
+parser.add_argument('--filter', '-f', default=1.333, type=float,
+                    help='smoothing filer FWHM [pix]')
 args = parser.parse_args()
 
 count   = args.count
 niter   = args.niter
 nsub    = args.nsub
+sfwhm   = args.filter
 
 # shepp-logan phantom
 image   = imread(data_dir + "/phantom.png", as_grey=True)
@@ -64,12 +67,16 @@ for iter in xrange(niter):
         bp  = iradon(ratio, theta=theta[views], filter=None, circle=True)
         recon   *= bp / (wgts[sub] + 1e-6)
 
+fbp = iradon(sinogram, theta=theta, circle=True)
+fbp = gaussian_filter(fbp, 1.333 / 2.355)
+recon   = gaussian_filter(recon, 1.333 / 2.355)
+
 # display
 plt.figure(figsize = (10, 5))
 plt.gray()
 plt.subplot(121)
 plt.title('FBP, ramp')
-plt.imshow(iradon(sinogram, theta=theta, circle=True), vmin = 0, vmax = 1)
+plt.imshow(fbp, vmin = 0, vmax = 1)
 plt.axis('off')
 plt.subplot(122)
 plt.title('OSEM, {} iteration, {} subset'.format(niter, nsub))
@@ -78,4 +85,15 @@ plt.axis('off')
 
 plt.subplots_adjust(0, 0, 1, 0.9, 0, 0.1)
 plt.savefig('osem.png')
+
+plt.figure()
+plt.suptitle('Profile of image')
+plt.plot(image[shape[0] / 2], label='Phantom')
+plt.plot(fbp[shape[0] / 2], label='FBP')
+plt.plot(recon[shape[0] / 2], label='OSEM')
+plt.legend(loc=3, prop={'size':10})
+plt.xlabel('X [pix]')
+plt.ylabel('[a.u.]')
+plt.savefig('profile.png')
+
 plt.show()
